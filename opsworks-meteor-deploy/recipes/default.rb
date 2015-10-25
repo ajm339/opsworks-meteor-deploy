9c65a5b0e4443e32e4d4c9ba44379d849dbf1fca
Alex Meyers <ajm339@cornell.edu>
2015-10-24 18:59:37 -0700

    new approach

--- opsworks-meteor-deploy/recipes/default.rb
+++ opsworks-meteor-deploy/recipes/default.rb
@@ -6,14 +6,31 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-execute "Change to Meteor Application Directory" do
-  command "cd #{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current"
+
+# execute "Change to Meteor Application Directory" do
+#   command "cd "
+# end
+
+# execute "Configure Meteor Dependencies with npm install" do
+#   command "(cd programs/server && npm install)"
+# end
+
+# execute "Start Meteor as Node Application" do
+#   command "node main.js"
+# end
+
+bash "meteor install" do 
+	user "root"
+	cwd "#{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current/programs/server"
+	code <<-EOH
+		npm install
+	EOH
 end
 
-execute "Configure Meteor Dependencies with npm install" do
-  command "(cd programs/server && npm install)"
-end
-
-execute "Start Meteor as Node Application" do
-  command "node main.js"
+bash "meteor start" do 
+	user "root"
+	cwd "#{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current"
+	code <<-EOH
+		node main.js
+	EOH
 end
