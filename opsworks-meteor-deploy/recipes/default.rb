937d2c6c040159dc06e9a1d8c21570904973503d
Alex Meyers <ajm339@cornell.edu>
2015-10-25 19:39:06 -0700

    remove commented code

--- opsworks-meteor-deploy/recipes/default.rb
+++ opsworks-meteor-deploy/recipes/default.rb
@@ -15,24 +15,7 @@ bash "meteor install" do
 	EOH
 end
 
-# service "meteor start" do
-# 	init_command "cd #{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current"
-# 	start_command "PORT=#{node["deploy"]["macrofuel_meteor"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_meteor"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_meteor"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_meteor"]["MAIL_URL"]} forever start main.js"
-# 	restart_command "PORT=#{node["deploy"]["macrofuel_meteor"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_meteor"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_meteor"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_meteor"]["MAIL_URL"]} forever restart main.js"
-# 	stop_command "forever stop main.js"
-# 	action [:start, :restart, :stop]
-# end
-
 execute "Start Meteor as Node Application" do
 	cwd "#{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current"
 	command "PORT=#{node["deploy"]["macrofuel_meteor"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_meteor"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_meteor"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_meteor"]["MAIL_URL"]} forever start main.js"
 end
-
-# bash "meteor start" do 
-# 	user "root"
-# 	cwd "#{node["deploy"]["macrofuel_meteor"]["deploy_to"]}/current"
-# 	timeout 10
-# 	code <<-EOH
-# 		PORT=#{node["deploy"]["macrofuel_meteor"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_meteor"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_meteor"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_meteor"]["MAIL_URL"]} node main.js
-# 	EOH
-# end
