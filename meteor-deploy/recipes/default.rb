da2b348d78937fbd14a54da48c5d7b0ca5116e7a
Alex Meyers <ajm339@cornell.edu>
2015-12-12 23:03:42 -0800

    fix app name

--- meteor-deploy/recipes/default.rb
+++ meteor-deploy/recipes/default.rb
@@ -16,6 +16,6 @@ bash "meteor install" do
 end
 
 execute "Start Meteor as Node Application" do
-	cwd "#{node["deploy"]["YOUR_APP_NAME"]["deploy_to"]}/current"
+	cwd "#{node["deploy"]["macrofuel_store"]["deploy_to"]}/current"
 	command "PORT=#{node["deploy"]["macrofuel_store"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_store"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_store"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_store"]["MAIL_URL"]} forever start main.js"
 end
