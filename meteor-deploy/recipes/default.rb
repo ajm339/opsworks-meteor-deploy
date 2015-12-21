15e11d1729319dcc2ac071a45f05a4b932ae47d0
Alex Meyers <ajm339@cornell.edu>
2015-12-20 16:31:31 -0800

    add DISABLE_WEBSOCKETS option

--- meteor-deploy/recipes/default.rb
+++ meteor-deploy/recipes/default.rb
@@ -15,7 +15,17 @@ bash "meteor install" do
 	EOH
 end
 
-execute "Start Meteor as Node Application" do
-	cwd "#{node["deploy"]["macrofuel_store"]["deploy_to"]}/current"
-	command "PORT=#{node["deploy"]["macrofuel_store"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_store"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_store"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_store"]["MAIL_URL"]} forever start main.js"
+if( defined?( node["deploy"]["macrofuel_store"]["DISABLE_WEBSOCKETS"] ) )
+
+	execute "Start Meteor as Node Application with Websockets enabled (no DISABLE_WEBSOCKETS option available)" do
+		cwd "#{node["deploy"]["macrofuel_store"]["deploy_to"]}/current"
+		command "PORT=#{node["deploy"]["macrofuel_store"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_store"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_store"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_store"]["MAIL_URL"]} forever start main.js"
+	end
+
+else
+
+	execute "Start Meteor as Node Application with Websockets option defined in Stack Settings" do
+		cwd "#{node["deploy"]["macrofuel_store"]["deploy_to"]}/current"
+		command "PORT=#{node["deploy"]["macrofuel_store"]["PORT"]} MONGO_URL=#{node["deploy"]["macrofuel_store"]["MONGO_URL"]} ROOT_URL=#{node["deploy"]["macrofuel_store"]["ROOT_URL"]} MAIL_URL=#{node["deploy"]["macrofuel_store"]["MAIL_URL"]} DISABLE_WEBSOCKETS=#{node["deploy"]["macrofuel_store"]["DISABLE_WEBSOCKETS"]} forever start main.js"
+		
 end
